5bd67a596fcbbfbd5141a263dbfa9f593c3cc3d1
unknown <sand8594@hufs.ac.kr>
2023-01-11 15:31:57 +0900

    css 추가 방법

--- nextjs-intro/components/NavBar.js
+++ nextjs-intro/components/NavBar.js
@@ -1,18 +1,28 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
   console.log(router)
   return (
     <nav>
-      <Link className={`${styles.link} ${router.pathname === "/" ? styles.active : ""}`} href="/">
-        Home
+      <Link  href="/" legacyBehavior >
+        <span className={router.pathname === "/" ? "active":""}>Home</span>
       </Link>
-      <Link className={[styles.link, router.pathname === "/about" ? styles.active : "",].join(" ")} href="/about">
-          About
+      <Link href="/about" legacyBehavior>
+          <span className={router.pathname === "/about" ? "active":""} >About</span>
       </Link>
+      <style jsx>{`
+        nav {
+          background-color:tomato;
+        }
+        span{
+          text-decoration: none;
+        }
+        .active {
+          color: yellow;
+        }
+      `}</style>
     </nav>
   );
 }
