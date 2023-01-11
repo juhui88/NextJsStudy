8c8d3ef96713a7e65fc98020104407a662c34169
unknown <sand8594@hufs.ac.kr>
2023-01-11 18:06:15 +0900

    NavBar custom

--- nextjs-intro/components/NavBar.js
+++ nextjs-intro/components/NavBar.js
@@ -13,12 +13,28 @@ export default function NavBar() {
       </Link>
       <style jsx>{`
         nav {
+          display: flex;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+          justify-content: center;
         }
         a{
           text-decoration: none;
+          padding:10px;
         }
         .active {
           color: tomato;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
         }
       `}</style>
     </nav>
