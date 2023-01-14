2984a4e1176de9ecd52ddf1347943e143b519404
juhui88 <sand8594@naver.com>
2023-01-14 16:52:06 +0900

    API key 숨기기

--- nextjs-intro/components/NavBar.js
+++ nextjs-intro/components/NavBar.js
@@ -5,37 +5,38 @@ export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
-      <Link  href="/" legacyBehavior >
-        <a className={router.pathname === "/" ? "active":""}>Home</a>
-      </Link>
-      <Link href="/about" legacyBehavior>
-          <a className={router.pathname === "/about" ? "active":""} >About</a>
-      </Link>
+      <img src = "/next.svg"></img>
+      <div>
+        <Link  href="/" legacyBehavior >
+          <span className={router.pathname === "/" ? "active":""}>Home</span>
+        </Link>
+        <Link href="/about" legacyBehavior>
+            <span className={router.pathname === "/about" ? "active":""} >About</span>
+        </Link>
+      </div>
+
       <style jsx>{`
-        nav {
-          display: flex;
-          align-items: center;
-          padding-top: 20px;
-          padding-bottom: 10px;
-          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+      nav {
+        font-size: 18px;
+        font-weight: bold;
+        margin: 0 auto;
+        text-align: center;
+        box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
             rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
-          justify-content: center;
-        }
-        a{
-          text-decoration: none;
-          padding:10px;
-        }
-        .active {
-          color: tomato;
-        }
-        nav a {
-          font-weight: 600;
-          font-size: 18px;
-        }
-        nav div {
-          display: flex;
-          gap: 10px;
-        }
+      }
+      .active {
+        color: tomato;
+      }
+      img {
+        margin: 20px 10px 10px 10px;
+        max-width: 80px;
+      }
+      div {
+        padding: 10px 20px;
+      }
+      span {
+        margin: 20px;
+      }
       `}</style>
     </nav>
   );
--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,10 +1,60 @@
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
+const API_KEY = "da8f266ea3c69d7a4a7bb4928de2cc69";
+
+
 export default function Home() {
+  const [movies, setMovies] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `api/movies`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
+
+
   return (
-    <div>
+    <div className="wrap">
       <Seo title = "Home"/>
-      <h1>Hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map(m => 
+      <div key = {m.id}>
+        <img src={`https://image.tmdb.org/t/p/w500/${m.poster_path}`}></img>
+        <p>{m.original_title}</p>
+      </div> )}
+    <style jsx>{`
+      .wrap {
+        display: grid;
+        grid-template-columns: repeat(2,1fr);
+        text-align: center;
+        padding: 20px 10px;
+      }
+      img {
+        width: 220px;
+        border-radius:10px;
+        box-shadow: rgba(0, 0, 0, 0.5) 0px 4px 12px;
+        transition: transform 0.2s ease-in-out;
+      }
+      img:hover {
+        transform: scale(1.04)
+      }
+      div {
+        padding-bottom: 50px;
+      }
+      p {
+        font-weight: bold;
+        font-family: cursive;
+        width: 200px;
+        margin: 15px auto;
+        text-align: center;
+      }
+    `}</style>
     </div>
   );
 }
