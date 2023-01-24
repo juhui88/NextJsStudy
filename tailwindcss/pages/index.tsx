eabd937c1103a3a567f92db527a7b221c638755c
juhui88 <sand8594@naver.com>
2023-01-24 16:38:56 +0900

    Test Drice part Two

--- tailwindcss/pages/index.tsx
+++ tailwindcss/pages/index.tsx
@@ -2,8 +2,8 @@ import type { NextPage } from "next"
 
 const Home:NextPage = () =>{
   return (
-    <div className="bg-slate-400 py-20 px-10 grid gap-10">
-      <div className="bg-white p-6 rounded-3xl shadow-xl">
+    <div className="bg-slate-400 py-20  grid gap-10 ">
+      <div className="bg-white p-6 rounded-3xl shadow-xl w-96 mx-auto">
         <span className="font-semibold text-3xl">Select Item</span>
         <div className="flex justify-between my-2">
           <span className="text-gray-500">Grey Chair</span>
@@ -25,9 +25,31 @@ const Home:NextPage = () =>{
           Checkout
         </div>
       </div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white overflow-hidden rounded-3xl shadow-xl w-96 mx-auto">
+        <div className="bg-blue-500 p-6 pb-14 flex justify-between">
+          <span className="text-white text-2xl">Profile</span>
+          <span>🛒</span>
+        </div>
+        <div className="rounded-3xl p-6 bg-white relative -top-5">
+          <div className="flex relative -top-16 items-end justify-between">
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Orders</span>
+              <span className="font-semibold">340</span>
+            </div>
+            <div className="h-24 w-24 bg-red-300 rounded-full" />
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Spent</span>
+              <span className="font-semibold">$340</span>
+            </div>
+          </div>
+          <div className="relative  flex flex-col items-center -mt-10 -mb-5">
+            <span className="text-lg font-semibold">Tony Molloy</span>
+            <span className="text-sm text-gray-500">New York, USA</span>
+          </div>
+        </div>
+      </div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl w-96 mx-auto"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl w-96 mx-auto"></div>
     </div>
   )
 }
